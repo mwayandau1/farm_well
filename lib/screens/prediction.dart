44d5b10abf46203faeba19cb6df95a90dab5c420
mwayandau1 <global-mosesayandau1@gmail.com>
2024-05-12 09:39:15 +0000

    Modified prediction to display predicted results

--- lib/screens/prediction.dart
+++ lib/screens/prediction.dart
@@ -11,15 +11,26 @@ class PredictionScreen extends StatefulWidget {
 
 class _PredictionScreenState extends State<PredictionScreen> {
   File? _imageFile;
+  String? _predictionResult;
 
   Future<void> _pickImage(ImageSource source) async {
     final pickedFile = await ImagePicker().pickImage(source: source);
     if (pickedFile != null) {
       setState(() {
         _imageFile = File(pickedFile.path);
+        _predictionResult = null; // Reset the prediction result
       });
       // Call your prediction function here
     }
+  }
+
+  void _predictDisease() {
+    // Call your prediction function here
+    // Set the _predictionResult based on the prediction logic
+    setState(() {
+      _predictionResult =
+          'Predicted Disease: Leaf Blight'; // Replace with actual prediction result
+    });
   }
 
   @override
@@ -49,65 +60,78 @@ class _PredictionScreenState extends State<PredictionScreen> {
                       width: 300,
                       fit: BoxFit.cover,
                     ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Take a photo of your plant',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+                  if (_imageFile == null)
+                    Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        const Text(
+                          'Take a photo of your plant',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          'Make sure the entire plant is in the frame and\nthat the image is clear.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton.icon(
+                          onPressed: () => _pickImage(ImageSource.camera),
+                          icon: Icon(Icons.camera_alt),
+                          label: Text('Use Camera'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextButton(
+                          onPressed: () => _pickImage(ImageSource.gallery),
+                          child: const Text(
+                            'Upload from gallery',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.green,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Make sure the entire plant is in the frame and\nthat the image is clear.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    onPressed: () => _pickImage(ImageSource.camera),
-                    icon: Icon(Icons.camera_alt),
-                    label: Text('Use Camera'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+                  if (_imageFile != null) ...[
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _predictDisease,
+                      child: Text('Predict'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  const Text(
-                    'How it works',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Our algorithm looks at the leaf, stem, and flower.\nIt then predicts the plant\'s disease based on the\ncolor, shape, and size of each part.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextButton(
-                    onPressed: () => _pickImage(ImageSource.gallery),
-                    child: const Text(
-                      'Upload from gallery',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.green,
-                        decoration: TextDecoration.underline,
+                    if (_predictionResult != null)
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          _predictionResult!,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
+                  ],
                 ],
               ),
             ),
