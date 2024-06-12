095a89e0462ae3cbf664c98556b1b38bd50e9a70
mwayandau1 <mosesayandau1@gmail.com>
2024-06-12 09:40:31 +0000

    Added search functionality to the all prediction screen and all educational content screen

--- lib/screens/all_educational_content_screen.dart
+++ lib/screens/all_educational_content_screen.dart
@@ -3,8 +3,34 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:farm_well/screens/educational_content_detail.dart';
 import 'package:farm_well/widgets/content_card.dart';
 
-class AllEducationalContentScreen extends StatelessWidget {
+class AllEducationalContentScreen extends StatefulWidget {
   const AllEducationalContentScreen({super.key});
+
+  @override
+  _AllEducationalContentScreenState createState() =>
+      _AllEducationalContentScreenState();
+}
+
+class _AllEducationalContentScreenState
+    extends State<AllEducationalContentScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  String _searchText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      setState(() {
+        _searchText = _searchController.text.toLowerCase();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,54 +38,79 @@ class AllEducationalContentScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("All Educational Content"),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection("educational_content")
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            List<DocumentSnapshot> documents = snapshot.data!.docs;
-            if (documents.isEmpty) {
-              return Center(
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "images/noData.png",
-                      width: 250,
-                    ),
-                    const Text("No Educational Content yet"),
-                  ],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search by title',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-              );
-            }
-            return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> data =
-                    documents[index].data() as Map<String, dynamic>;
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EducationalContentDetail(
-                          title: data['title'],
-                          content: data['content'],
-                          image: data['image'],
-                        ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("educational_content")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  if (_searchText.isNotEmpty) {
+                    documents = documents.where((doc) {
+                      String title = (doc['title'] as String).toLowerCase();
+                      return title.contains(_searchText);
+                    }).toList();
+                  }
+                  if (documents.isEmpty) {
+                    return Center(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "images/noData.png",
+                            width: 250,
+                          ),
+                          const Text("No Educational Content yet"),
+                        ],
                       ),
                     );
-                  },
-                  child: ContentCard(data: data),
-                );
+                  }
+                  return ListView.builder(
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> data =
+                          documents[index].data() as Map<String, dynamic>;
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => EducationalContentDetail(
+                                title: data['title'],
+                                content: data['content'],
+                                image: data['image'],
+                              ),
+                            ),
+                          );
+                        },
+                        child: ContentCard(data: data),
+                      );
+                    },
+                  );
+                }
               },
-            );
-          }
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/all_prediction_screen.dart
+++ lib/screens/all_prediction_screen.dart
@@ -3,8 +3,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:farm_well/screens/prediction_detail.dart';
 import 'package:farm_well/widgets/prediction_card.dart';
 
-class AllPredictionsScreen extends StatelessWidget {
+class AllPredictionsScreen extends StatefulWidget {
   const AllPredictionsScreen({super.key});
+
+  @override
+  _AllPredictionsScreenState createState() => _AllPredictionsScreenState();
+}
+
+class _AllPredictionsScreenState extends State<AllPredictionsScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  String _searchText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      setState(() {
+        _searchText = _searchController.text.toLowerCase();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,53 +36,79 @@ class AllPredictionsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("All Predictions"),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection("predictions")
-            .orderBy('timestamp', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return Center(
-              child: Column(
-                children: [
-                  Image.asset(
-                    "images/noData.png",
-                    width: 250,
-                  ),
-                  const Text("No Prediction Results yet"),
-                ],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search by prediction',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
               ),
-            );
-          } else {
-            List<DocumentSnapshot> documents = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> data =
-                    documents[index].data() as Map<String, dynamic>;
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PredictionDetail(
-                          prediction: data['prediction'],
-                          imagePath: data['image_url'],
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("predictions")
+                  .orderBy('timestamp', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "images/noData.png",
+                          width: 250,
                         ),
-                      ),
-                    );
-                  },
-                  child: PredictionCard(data: data),
-                );
+                        const Text("No Prediction Results yet"),
+                      ],
+                    ),
+                  );
+                } else {
+                  List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  if (_searchText.isNotEmpty) {
+                    documents = documents.where((doc) {
+                      String prediction =
+                          (doc['prediction'] as String).toLowerCase();
+                      return prediction.contains(_searchText);
+                    }).toList();
+                  }
+                  return ListView.builder(
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> data =
+                          documents[index].data() as Map<String, dynamic>;
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PredictionDetail(
+                                prediction: data['prediction'],
+                                imagePath: data['image_url'],
+                              ),
+                            ),
+                          );
+                        },
+                        child: PredictionCard(data: data),
+                      );
+                    },
+                  );
+                }
               },
-            );
-          }
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
